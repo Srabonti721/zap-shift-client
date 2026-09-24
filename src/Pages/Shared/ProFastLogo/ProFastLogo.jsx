6984918bb170e20f394e7077acd604b75a170eed
Srabonti <somayaaktarshabonti721@gmail.com>
2026-09-25 01:11:00 +0600

    add navbar , footer and website logo

---
+++ src/Pages/Shared/ProFastLogo/ProFastLogo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import logo from "../../../assets/logo.png"
+
+const ProFastLogo = () => {
+  return (
+    <div>
+        <img src={logo} alt="" />
+      <h2>ProFast</h2>
+    </div>
+  )
+}
+
+export default ProFastLogo
